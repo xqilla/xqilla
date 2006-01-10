81b06c1aa9ec391c714611c2db2900bdbda53d4d
John Snelson <jpcs@users.sourceforge.net>
2006-01-10 16:41:33 +0000

    Implemented the ability to save a list of known errors, and report when tests that were expected to fail actually pass, or vice versa. Also allows test failures to be reclassified as "pass" or "skip" - useful if the test uses a feature that isn't supported, or if you want to mark "inspect" tests as "pass" after checking them.
    Also implemented XML test result output, in the format required to submit
    test results to the W3C.

--- include/xqilla/xqts/TestSuiteResultListener.hpp
+++ include/xqilla/xqts/TestSuiteResultListener.hpp
@@ -28,6 +28,8 @@
 
 #include <string>
 #include <sstream>
+#include <map>
+#include <vector>
 
 #include <xqilla/framework/XQillaExport.hpp>
 
@@ -44,7 +46,7 @@ public:
   virtual void reportPass(const TestCase &testCase) = 0;
   virtual void reportInspect(const TestCase &testCase, const std::string &actualResult,
                              const std::string &expectedResult) = 0;
-  virtual void reportSkip(const TestCase &testCase) = 0;
+  virtual void reportSkip(const TestCase &testCase, const std::string &comment) = 0;
   virtual void reportFail(const TestCase &testCase, const std::string &actualResult,
                           const std::string &expectedResult) = 0;
   virtual void reportFailNoError(const TestCase &testCase, const std::string &actualResult) = 0;
@@ -52,6 +54,44 @@ public:
 
 protected:
   TestSuiteResultListener() {}
+};
+
+class XQILLA_API KnownErrorChecker : public TestSuiteResultListener
+{
+public:
+  KnownErrorChecker(TestSuiteResultListener *results);
+
+  virtual void startTestGroup(const std::string &name);
+  virtual void endTestGroup();
+
+  virtual void reportPass(const TestCase &testCase);
+  virtual void reportInspect(const TestCase &testCase, const std::string &actualResult,
+                             const std::string &expectedResult);
+  virtual void reportSkip(const TestCase &testCase, const std::string &comment);
+  virtual void reportFail(const TestCase &testCase, const std::string &actualResult,
+                          const std::string &expectedResult);
+  virtual void reportFailNoError(const TestCase &testCase, const std::string &actualResult);
+  virtual void reportFailUnexpectedError(const TestCase &testCase, const std::string &unexpectedError);
+
+  void printReport() const;
+
+  bool loadErrors(const std::string &errorFile);
+  bool saveErrors(const std::string &errorFile) const;
+
+  class Error {
+  public:
+    std::string reason;
+    std::string comment;
+    std::string action;
+  };
+
+private:
+  TestSuiteResultListener *results_;
+
+  // testName -> (reason, comment, action)
+  std::map<std::string, Error> errors_;
+  std::vector<std::string> nowPass_;
+  std::vector<std::string> nowFail_;  
 };
 
 class XQILLA_API ConsoleResultListener : public TestSuiteResultListener
@@ -65,7 +105,7 @@ public:
   virtual void reportPass(const TestCase &testCase);
   virtual void reportInspect(const TestCase &testCase, const std::string &actualResult,
                              const std::string &expectedResult);
-  virtual void reportSkip(const TestCase &testCase);
+  virtual void reportSkip(const TestCase &testCase, const std::string &comment);
   virtual void reportFail(const TestCase &testCase, const std::string &actualResult,
                           const std::string &expectedResult);
   virtual void reportFailNoError(const TestCase &testCase, const std::string &actualResult);
@@ -74,7 +114,7 @@ public:
   void printReport() const;
 
 private:
-  void testCaseToErrors(const TestCase &testCase);
+  void testCaseToErrorStream(const TestCase &testCase);
 
 private:
   std::string m_szFullTestName;
@@ -84,7 +124,47 @@ private:
   int testDepth_;
   bool needNewline_;
 
-  std::ostringstream errors_;
+  std::ostringstream errorStream_;
+};
+
+class XQILLA_API XMLReportResultListener : public TestSuiteResultListener
+{
+public:
+  XMLReportResultListener();
+
+  virtual void startTestGroup(const std::string &name);
+  virtual void endTestGroup();
+
+  virtual void reportPass(const TestCase &testCase);
+  virtual void reportInspect(const TestCase &testCase, const std::string &actualResult,
+                             const std::string &expectedResult);
+  virtual void reportSkip(const TestCase &testCase, const std::string &comment);
+  virtual void reportFail(const TestCase &testCase, const std::string &actualResult,
+                          const std::string &expectedResult);
+  virtual void reportFailNoError(const TestCase &testCase, const std::string &actualResult);
+  virtual void reportFailUnexpectedError(const TestCase &testCase, const std::string &unexpectedError);
+
+  void setImplementation(const std::string &name, const std::string &version = "", const std::string &description = "");
+  void setOrganization(const std::string &name, const std::string &website = "");
+  void setSubmittor(const std::string &name, const std::string &email = "");
+  void setTestRun(const std::string &transformation, const std::string &comments = "");
+  void addImplementationDefinedItem(const std::string &name, const std::string &value);
+  void addFeature(const std::string &name, bool supported);
+  void addContextProperty(const std::string &name, const std::string &contextType, const std::string &value);
+
+  void printReport() const;
+
+private:
+  std::string implName_, implVersion_, implDescription_;
+  std::string orgName_, orgWebsite_;
+  std::string submittorName_, submittorEmail_;
+  std::string testTransformation_, testComments_;
+
+  std::map<std::string, std::string> implDefinedItems_;
+  std::map<std::string, bool> features_;
+  std::map<std::string, std::pair<std::string, std::string> > contextProperties_;
+
+  std::ostringstream outputStream_;
 };
 
 #endif
--- src/xqts/TestSuiteResultListener.cpp
+++ src/xqts/TestSuiteResultListener.cpp
@@ -27,8 +27,205 @@
 #include <xqilla/xqts/TestSuiteParser.hpp>
 
 #include <iostream>
+#include <fstream>
+#include <time.h>
 
+#include <xercesc/parsers/SAXParser.hpp>
+#include <xercesc/sax/AttributeList.hpp>
+#include <xercesc/framework/LocalFileInputSource.hpp>
+
+#include <xqilla/utils/XStr.hpp>
+#include <xqilla/utils/UTF8Str.hpp>
+
+#if defined(XERCES_HAS_CPP_NAMESPACE)
+XERCES_CPP_NAMESPACE_USE
+#endif
 using namespace std;
+
+KnownErrorChecker::KnownErrorChecker(TestSuiteResultListener *results)
+  : results_(results)
+{
+}
+
+void KnownErrorChecker::startTestGroup(const std::string &name)
+{
+  results_->startTestGroup(name);
+}
+
+void KnownErrorChecker::endTestGroup()
+{
+  results_->endTestGroup();
+}
+
+void KnownErrorChecker::reportPass(const TestCase &testCase)
+{
+  map<string, Error>::iterator i = errors_.find(testCase.name);
+  if(i != errors_.end()) {
+    nowPass_.push_back(testCase.name);
+    errors_.erase(i);
+  }
+
+  results_->reportPass(testCase);
+}
+
+void KnownErrorChecker::reportInspect(const TestCase &testCase, const string &actualResult,
+                                      const string &expectedResult)
+{
+  map<string, Error>::iterator i = errors_.find(testCase.name);
+  if(i != errors_.end()) {
+    if(i->second.action == "skip") {
+      results_->reportSkip(testCase, i->second.comment);
+      return;
+    }
+    else if(i->second.action == "pass") {
+      results_->reportPass(testCase);
+      return;
+    }
+  }
+  errors_[testCase.name].reason = "inspect";
+
+  results_->reportInspect(testCase, actualResult, expectedResult);
+}
+
+void KnownErrorChecker::reportSkip(const TestCase &testCase, const std::string &comment)
+{
+  results_->reportSkip(testCase, comment);
+}
+
+void KnownErrorChecker::reportFail(const TestCase &testCase, const string &actualResult,
+                                   const string &expectedResult)
+{
+  map<string, Error>::iterator i = errors_.find(testCase.name);
+  if(i == errors_.end()) {
+    nowFail_.push_back(testCase.name);
+  }
+  else if(i->second.action == "skip") {
+    results_->reportSkip(testCase, i->second.comment);
+    return;
+  }
+  errors_[testCase.name].reason = "result failure";
+
+  results_->reportFail(testCase, actualResult, expectedResult);
+}
+
+void KnownErrorChecker::reportFailNoError(const TestCase &testCase, const string &actualResult)
+{
+  map<string, Error>::iterator i = errors_.find(testCase.name);
+  if(i == errors_.end()) {
+    nowFail_.push_back(testCase.name);
+  }
+  else if(i->second.action == "skip") {
+    results_->reportSkip(testCase, i->second.comment);
+    return;
+  }
+  errors_[testCase.name].reason = "no error failure";
+
+  results_->reportFailNoError(testCase, actualResult);
+}
+
+void KnownErrorChecker::reportFailUnexpectedError(const TestCase &testCase, const string &unexpectedError)
+{
+  map<string, Error>::iterator i = errors_.find(testCase.name);
+  if(i == errors_.end()) {
+    nowFail_.push_back(testCase.name);
+  }
+  else if(i->second.action == "skip") {
+    results_->reportSkip(testCase, i->second.comment);
+    return;
+  }
+  errors_[testCase.name].reason = "error failure";
+
+  results_->reportFailUnexpectedError(testCase, unexpectedError);
+}
+
+void KnownErrorChecker::printReport() const
+{
+  if(!nowFail_.empty() || !nowPass_.empty()) {
+    cout << "************************************************************************" << endl;
+    if(!nowFail_.empty()) {
+      cout << nowFail_.size() << " unexpected failures:" << endl;
+      for(vector<string>::const_iterator i = nowFail_.begin(); i != nowFail_.end(); ++i) {
+        cout << "\t" << *i << endl;
+      }
+    }
+
+    if(!nowPass_.empty()) {
+      cout << nowPass_.size() << " unexpected passes:" << endl;
+      for(vector<string>::const_iterator j = nowPass_.begin(); j != nowPass_.end(); ++j) {
+        cout << "\t" << *j << endl;
+      }
+    }
+  }
+}
+
+class ErrorFileHandler : public XERCES_CPP_NAMESPACE_QUALIFIER HandlerBase
+{
+public:
+  ErrorFileHandler(map<string, KnownErrorChecker::Error> &errors)
+    : errors_(errors) {}
+
+  virtual void startElement(const XMLCh* const name, AttributeList &attributes)
+  {
+    string szName = UTF8(name);
+    if(szName == "error") {
+      KnownErrorChecker::Error &error = errors_[UTF8(attributes.getValue("id"))];
+      error.reason = UTF8(attributes.getValue("reason"));
+      error.comment = UTF8(attributes.getValue("comment"));
+      error.action = UTF8(attributes.getValue("action"));
+    }
+  }
+
+private:
+  map<string, KnownErrorChecker::Error> &errors_;
+};
+
+
+bool KnownErrorChecker::loadErrors(const std::string &errorFile)
+{
+  try {
+    SAXParser parser;
+    ErrorFileHandler handler(errors_);
+    parser.setDocumentHandler(&handler);
+    LocalFileInputSource is(X(errorFile.c_str()));
+    parser.parse(is);
+  }
+  catch(const XMLException& e) {
+    cout << "Error while reading error file: " << UTF8(e.getMessage()) << endl;
+    return false;
+  }
+
+  return true;
+}
+
+bool KnownErrorChecker::saveErrors(const std::string &errorFile) const
+{
+  ofstream file(errorFile.c_str());
+  if(!file.is_open()) return false;
+
+  file << "<errors>" << endl;
+
+  for(map<string, Error>::const_iterator i = errors_.begin();
+      i != errors_.end(); ++i) {
+    file << "  <error id=\"" << i->first
+         << "\" reason=\"" << i->second.reason << "\"";
+
+    if(i->second.action != "")
+      file << " action=\"" << i->second.action << "\"";
+
+    if(i->second.comment != "")
+      file << " comment=\"" << i->second.comment << "\"";
+
+    file << "/>" << endl;
+  }
+
+  file << "</errors>" << endl;
+
+  file.close();
+
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ConsoleResultListener::ConsoleResultListener()
   : m_nTotalTests(0),
@@ -75,36 +272,6 @@ void ConsoleResultListener::reportPass(const TestCase &testCase)
   cout << "." << flush;
 }
 
-void ConsoleResultListener::testCaseToErrors(const TestCase &testCase)
-{
-  errors_ << "* Test-case '" << m_szFullTestName << ":" << testCase.name << "':" << endl;
-  errors_ << "* Query URL: " << testCase.queryURL << endl;
-
-  for(std::map<std::string, std::string>::const_iterator i = testCase.inputVars.begin();
-      i != testCase.inputVars.end(); ++i) {
-    errors_ << "* Input: " << i->first << " -> " << i->second << endl;
-  }
-
-  for(std::map<std::string, std::string>::const_iterator j = testCase.outputFiles.begin();
-      j != testCase.outputFiles.end(); ++j) {
-    errors_ << "* Output: " << j->second << " -> " << j->first << endl;
-  }
-
-  if(!testCase.expectedErrors.empty()) {
-    errors_ << "* Errors:";
-    for(std::list<std::string>::const_iterator k = testCase.expectedErrors.begin();
-        k != testCase.expectedErrors.end(); ++k) {
-      errors_ << " " << *k;
-    }
-    errors_ << endl;
-  }
-
-  errors_ << endl;
-  errors_ << "********** Query: **********" << endl;
-  errors_ << testCase.query << endl;
-  errors_ << endl;
-}
-
 void ConsoleResultListener::reportInspect(const TestCase &testCase, const string &actualResult,
                                           const string &expectedResult)
 {
@@ -113,18 +280,18 @@ void ConsoleResultListener::reportInspect(const TestCase &testCase, const string
 
   cout << "," << flush;
 
-  errors_ << "************************************************************************" << endl;
-  errors_ << "* For inspection:" << endl;
-  testCaseToErrors(testCase);
-  errors_ << "********** Actual result: **********" << endl;
-  errors_ << actualResult << endl;;
-  errors_ << endl;
-  errors_ << "********** Expected result: **********" << endl;;
-  errors_ << expectedResult << endl;
-  errors_ << endl;
+  errorStream_ << "************************************************************************" << endl;
+  errorStream_ << "* For inspection:" << endl;
+  testCaseToErrorStream(testCase);
+  errorStream_ << "********** Actual result: **********" << endl;
+  errorStream_ << actualResult << endl;;
+  errorStream_ << endl;
+  errorStream_ << "********** Expected result: **********" << endl;;
+  errorStream_ << expectedResult << endl;
+  errorStream_ << endl;
 }
 
-void ConsoleResultListener::reportSkip(const TestCase &testCase)
+void ConsoleResultListener::reportSkip(const TestCase &testCase, const std::string &comment)
 {
   ++m_nTotalTests;
   ++m_nSkippedTests;
@@ -139,14 +306,14 @@ void ConsoleResultListener::reportFail(const TestCase &testCase, const string &a
 
   cout << "!" << flush;
 
-  errors_ << "************************************************************************" << endl;
-  testCaseToErrors(testCase);
-  errors_ << "********** Actual result: **********" << endl;
-  errors_ << actualResult << endl;;
-  errors_ << endl;
-  errors_ << "********** Expected result: **********" << endl;;
-  errors_ << expectedResult << endl;
-  errors_ << endl;
+  errorStream_ << "************************************************************************" << endl;
+  testCaseToErrorStream(testCase);
+  errorStream_ << "********** Actual result: **********" << endl;
+  errorStream_ << actualResult << endl;;
+  errorStream_ << endl;
+  errorStream_ << "********** Expected result: **********" << endl;;
+  errorStream_ << expectedResult << endl;
+  errorStream_ << endl;
 }
 
 void ConsoleResultListener::reportFailNoError(const TestCase &testCase, const string &actualResult)
@@ -155,11 +322,11 @@ void ConsoleResultListener::reportFailNoError(const TestCase &testCase, const st
 
   cout << "!" << flush;
 
-  errors_ << "************************************************************************" << endl;
-  testCaseToErrors(testCase);
-  errors_ << "********** Actual result: **********" << endl;
-  errors_ << actualResult << endl;;
-  errors_ << endl;
+  errorStream_ << "************************************************************************" << endl;
+  testCaseToErrorStream(testCase);
+  errorStream_ << "********** Actual result: **********" << endl;
+  errorStream_ << actualResult << endl;;
+  errorStream_ << endl;
 }
 
 void ConsoleResultListener::reportFailUnexpectedError(const TestCase &testCase, const string &unexpectedError)
@@ -168,11 +335,11 @@ void ConsoleResultListener::reportFailUnexpectedError(const TestCase &testCase, 
 
   cout << "!" << flush;
 
-  errors_ << "************************************************************************" << endl;
-  testCaseToErrors(testCase);
-  errors_ << "********** Actual error: **********" << endl;
-  errors_ << unexpectedError << endl;;
-  errors_ << endl;
+  errorStream_ << "************************************************************************" << endl;
+  testCaseToErrorStream(testCase);
+  errorStream_ << "********** Actual error: **********" << endl;
+  errorStream_ << unexpectedError << endl;;
+  errorStream_ << endl;
 }
 
 void ConsoleResultListener::printReport() const
@@ -180,10 +347,225 @@ void ConsoleResultListener::printReport() const
   cout << "************************************************************************" << endl;
   cout << m_nTotalTests << " Tests, "
        << m_nPassedTests << " passed, "
+       << (m_nTotalTests - m_nPassedTests - m_nSkippedTests - m_nInspectTests) << " failed, "
        << m_nSkippedTests << " skipped, "
        << m_nInspectTests << " for inspection";
   cout << " (" << ((float)m_nPassedTests)/(m_nTotalTests-m_nSkippedTests-m_nInspectTests)*100 << "%)" << endl;
 
-  cerr << errors_.str();
-  cout << "************************************************************************" << endl;
+  cerr << errorStream_.str();
+}
+
+void ConsoleResultListener::testCaseToErrorStream(const TestCase &testCase)
+{
+  errorStream_ << "* Test-case '" << m_szFullTestName << ":" << testCase.name << "':" << endl;
+  errorStream_ << "* Query URL: " << testCase.queryURL << endl;
+
+  for(std::map<std::string, std::string>::const_iterator i = testCase.inputVars.begin();
+      i != testCase.inputVars.end(); ++i) {
+    errorStream_ << "* Input: " << i->first << " -> " << i->second << endl;
+  }
+
+  for(std::map<std::string, std::string>::const_iterator j = testCase.outputFiles.begin();
+      j != testCase.outputFiles.end(); ++j) {
+    errorStream_ << "* Output: " << j->second << " -> " << j->first << endl;
+  }
+
+  if(!testCase.expectedErrors.empty()) {
+    errorStream_ << "* Errors:";
+    for(std::list<std::string>::const_iterator k = testCase.expectedErrors.begin();
+        k != testCase.expectedErrors.end(); ++k) {
+      errorStream_ << " " << *k;
+    }
+    errorStream_ << endl;
+  }
+
+  errorStream_ << endl;
+  errorStream_ << "********** Query: **********" << endl;
+  errorStream_ << testCase.query << endl;
+  errorStream_ << endl;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+XMLReportResultListener::XMLReportResultListener()
+{
+}
+
+void XMLReportResultListener::startTestGroup(const std::string &name)
+{
+}
+
+void XMLReportResultListener::endTestGroup()
+{
+}
+
+void XMLReportResultListener::reportPass(const TestCase &testCase)
+{
+  outputStream_ << "  <test-case";
+  outputStream_ << " name=\"" << testCase.name << "\"";
+  outputStream_ << " result=\"pass\"";
+  outputStream_ << "/>" << endl;
+}
+
+void XMLReportResultListener::reportInspect(const TestCase &testCase, const std::string &actualResult,
+                                            const std::string &expectedResult)
+{
+  outputStream_ << "  <test-case";
+  outputStream_ << " name=\"" << testCase.name << "\"";
+  outputStream_ << " result=\"cannot tell\"";
+  outputStream_ << " comment=\"result has not been inspected\"";
+  outputStream_ << "/>" << endl;
+}
+
+void XMLReportResultListener::reportSkip(const TestCase &testCase, const std::string &comment)
+{
+  outputStream_ << "  <test-case";
+  outputStream_ << " name=\"" << testCase.name << "\"";
+  outputStream_ << " result=\"not applicable\"";
+  if(comment != "")
+    outputStream_ << " comment=\"" << comment << "\"";
+  outputStream_ << "/>" << endl;
+}
+
+void XMLReportResultListener::reportFail(const TestCase &testCase, const std::string &actualResult,
+                                         const std::string &expectedResult)
+{
+  outputStream_ << "  <test-case";
+  outputStream_ << " name=\"" << testCase.name << "\"";
+  outputStream_ << " result=\"fail\"";
+  outputStream_ << "/>" << endl;
+}
+
+void XMLReportResultListener::reportFailNoError(const TestCase &testCase, const std::string &actualResult)
+{
+  outputStream_ << "  <test-case";
+  outputStream_ << " name=\"" << testCase.name << "\"";
+  outputStream_ << " result=\"fail\"";
+  outputStream_ << "/>" << endl;
+}
+
+void XMLReportResultListener::reportFailUnexpectedError(const TestCase &testCase, const std::string &unexpectedError)
+{
+  outputStream_ << "  <test-case";
+  outputStream_ << " name=\"" << testCase.name << "\"";
+  outputStream_ << " result=\"fail\"";
+  outputStream_ << "/>" << endl;
+}
+
+void XMLReportResultListener::setImplementation(const std::string &name, const std::string &version, const std::string &description)
+{
+  implName_ = name;
+  implVersion_ = version;
+  implDescription_ = description;
+}
+
+void XMLReportResultListener::setOrganization(const std::string &name, const std::string &website)
+{
+  orgName_ = name;
+  orgWebsite_ = website;
+}
+
+void XMLReportResultListener::setSubmittor(const std::string &name, const std::string &email)
+{
+  submittorName_ = name;
+  submittorEmail_ = email;
+}
+
+void XMLReportResultListener::setTestRun(const std::string &transformation, const std::string &comments)
+{
+  testTransformation_ = transformation;
+  testComments_ = comments;
+}
+
+void XMLReportResultListener::addImplementationDefinedItem(const std::string &name, const std::string &value)
+{
+  implDefinedItems_[name] = value;
+}
+
+void XMLReportResultListener::addFeature(const std::string &name, bool supported)
+{
+  features_[name] = supported;
+}
+
+void XMLReportResultListener::addContextProperty(const std::string &name, const std::string &contextType, const std::string &value)
+{
+  contextProperties_[name].first = contextType;
+  contextProperties_[name].second = value;
+}
+
+void XMLReportResultListener::printReport() const
+{
+  cout << "<test-suite-result xmlns=\"http://www.w3.org/2005/02/query-test-XQTSResult\">" << endl;
+  cout << endl;
+  cout << "  <implementation name=\"" << implName_ << "\"";
+  if(implVersion_ != "")
+    cout << " version=\"" << implVersion_ << "\"";
+  cout << ">" << endl;
+  cout << endl;
+  cout << "    <organization name=\"" << orgName_ << "\"";
+  if(orgWebsite_ != "")
+    cout << " website=\"" << orgWebsite_ << "\"";
+  cout << "/>" << endl;
+  cout << endl;
+  cout << "    <submittor name=\"" << submittorName_ << "\"";
+  if(submittorEmail_ != "")
+    cout << " email=\"" << submittorEmail_ << "\"";
+  cout << "/>" << endl;
+  cout << endl;
+  if(implDescription_ != "") {
+    cout << "    <description>" << implDescription_ << "</description>" << endl;
+    cout << endl;
+  }
+
+  if(!implDefinedItems_.empty()) {
+    cout << "    <implementation-defined-items>" << endl;
+    for(map<string, string>::const_iterator i = implDefinedItems_.begin(); i != implDefinedItems_.end(); ++i) {
+      cout << "      <implementation-defined-item name=\"" << i->first << "\" value=\"" << i->second << "\"/>" << endl;
+    }
+    cout << "    </implementation-defined-items>" << endl;
+    cout << endl;
+  }
+
+  if(!features_.empty()) {
+    cout << "    <features>" << endl;
+    for(map<string, bool>::const_iterator i = features_.begin(); i != features_.end(); ++i) {
+      cout << "      <feature name=\"" << i->first << "\" supported=\"" << (i->second ? "true" : "false") << "\"/>" << endl;
+    }
+    cout << "    </features>" << endl;
+    cout << endl;
+  }
+
+  if(!contextProperties_.empty()) {
+    cout << "    <context-properties>" << endl;
+    for(map<string, pair<string, string> >::const_iterator i = contextProperties_.begin(); i != contextProperties_.end(); ++i) {
+      cout << "      <context-property name=\"" << i->first << "\" context-type=\"" << i->second.first << "\" value=\"" << i->second.second << "\"/>" << endl;
+    }
+    cout << "    </context-properties>" << endl;
+    cout << endl;
+  }
+
+  cout << "  </implementation>" << endl;
+  cout << endl;
+  cout << "  <syntax>XQuery</syntax>" << endl;
+  cout << endl;
+
+  time_t currentTime = ::time(0);
+  struct tm *curLocalDate = ::localtime(&currentTime);
+  char szDate[256];
+  snprintf(szDate, 256,"%04d-%02d-%02d",curLocalDate->tm_year+1900, curLocalDate->tm_mon+1, curLocalDate->tm_mday);
+
+  cout << "  <test-run dateRun=\"" << szDate << "\">" << endl;
+  cout << "    <test-suite version=\"0.8.4\"/>" << endl;
+  if(testTransformation_ != "")
+    cout << "    <transformation>" << testTransformation_ << "</transformation>" << endl;
+  cout << "    <comparison><p>XML and fragment comparisons are performed by serializing the results,";
+  cout << " re-parsing them, and using a custom deep-equal like function. This function currently ignores";
+  cout << " any text nodes that consist entirely of whitespace.</p></comparison>" << endl;
+  if(testComments_ != "")
+    cout << "    <otherComments>" << testComments_ << "</otherComments>" << endl;
+  cout << "  </test-run>" << endl;
+  cout << endl;
+  cout << outputStream_.str();
+  cout << endl;
+  cout << "</test-suite-result>" << endl;
 }
