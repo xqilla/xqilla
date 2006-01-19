7854fd6e89c6f977408913f17675ecab7a075961
John Snelson <jpcs@users.sourceforge.net>
2006-01-19 18:28:40 +0000

    Fixed a bug in XQDOMConstructor, where the default element namespace was not being set by a direct element constructor with an "xmlns" attribute.
    --HG--
    branch : xqilla

--- include/xqilla/context/XQScopedNamespace.hpp
+++ include/xqilla/context/XQScopedNamespace.hpp
@@ -30,36 +30,40 @@ XERCES_CPP_NAMESPACE_END
 class XQILLA_API XQScopedNamespace : public XQillaNSResolverImpl
 {
 public:
-	XQScopedNamespace(XPath2MemoryManager* memMgr, const XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* prevScope);
-	virtual ~XQScopedNamespace();
+  XQScopedNamespace(XPath2MemoryManager* memMgr, const XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* prevScope);
+  virtual ~XQScopedNamespace();
 
-    virtual const XMLCh* lookupNamespaceURI(const XMLCh* prefix) const;
-    virtual const XMLCh* lookupPrefix(const XMLCh* uri) const;
+  virtual const XMLCh* lookupNamespaceURI(const XMLCh* prefix) const;
+  virtual const XMLCh* lookupPrefix(const XMLCh* uri) const;
 
-    void setNodeContext(const XERCES_CPP_NAMESPACE_QUALIFIER DOMNode* node);
+  void setNodeContext(const XERCES_CPP_NAMESPACE_QUALIFIER DOMNode* node);
 protected:
-    const XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* _prevScope;
-    const XERCES_CPP_NAMESPACE_QUALIFIER DOMNode* _ctxNode;
+  const XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* _prevScope;
+  const XERCES_CPP_NAMESPACE_QUALIFIER DOMNode* _ctxNode;
 };
 
 class XQILLA_API AutoNsScopeReset
 {
 public:
-    AutoNsScopeReset(StaticContext* context, XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* newResolver)
-    {
-        _context=context;
-        _oldNSResolver=_context->getNSResolver();
-        _context->setNSResolver(newResolver);
-    }
+  AutoNsScopeReset(StaticContext* context, XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* newResolver)
+  {
+    _context=context;
+    _oldNSResolver=_context->getNSResolver();
+    _defaultElementAndTypeNS=context->getDefaultElementAndTypeNS();
+    _context->setNSResolver(newResolver);
+  }
 
-    ~AutoNsScopeReset()
-    {
-        _context->setNSResolver(_oldNSResolver);
-    }
+  ~AutoNsScopeReset()
+  {
+    _context->setNSResolver(_oldNSResolver);
+    _context->setDefaultElementAndTypeNS(_defaultElementAndTypeNS);
+  }
 
 protected:
-    StaticContext* _context;
-    const XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* _oldNSResolver;
+  StaticContext* _context;
+  const XERCES_CPP_NAMESPACE_QUALIFIER DOMXPathNSResolver* _oldNSResolver;
+  const XMLCh *_defaultElementAndTypeNS;
+  
 };
 
 #endif // !defined(AFXQ_XQSCOPEDNAMESPACE_H__D6A320F5_21F1_421D_9E46_E4373B375E1A__INCLUDED_)
