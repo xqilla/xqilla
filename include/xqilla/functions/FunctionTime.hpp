28b298cb60f88291c31f8838cd5bbb8e7d866bec
John Snelson <jpcs@users.sourceforge.net>
2008-01-31 17:37:16 +0000

    Added an xqilla:time() function which behaves like trace() but outputs the cumulative time spent evaluating it's arguments expression, rather than the expression's value. Unlike trace(), xqilla:time() does not interfere with lazy evaluation.
    --HG--
    branch : xqilla

--- include/xqilla/functions/FunctionTime.hpp
+++ include/xqilla/functions/FunctionTime.hpp
@@ -19,48 +19,29 @@
  * $Id$
  */
 
-#ifndef _XQPARENTHESIZEDEXPR_HPP
-#define _XQPARENTHESIZEDEXPR_HPP
+#ifndef _FUNCTIONTIME_HPP
+#define _FUNCTIONTIME_HPP
 
-#include <xqilla/framework/XQillaExport.hpp>
+#include "XQillaFunction.hpp"
 
-#include <xqilla/ast/ASTNodeImpl.hpp>
+class QueryPathNode;
 
-class XQILLA_API XQParenthesizedExpr : public ASTNodeImpl
+class XQILLA_API FunctionTime : public XQillaFunction
 {
-
 public:
-  XQParenthesizedExpr(XPath2MemoryManager* memMgr);	
+  static const XMLCh name[];
+  static const unsigned int minArgs;
+  static const unsigned int maxArgs;
 
-  ///adds a ASTNode to this XQParenthesizedExpr
-  void addItem(ASTNode* di);
+  FunctionTime(const VectorOfASTNodes &args, XPath2MemoryManager *memMgr);
+  
+  virtual ASTNode *staticResolution(StaticContext *context);
+  virtual ASTNode *staticTyping(StaticContext *context);
 
   virtual Result createResult(DynamicContext* context, int flags=0) const;
   virtual void generateEvents(EventHandler *events, DynamicContext *context,
                               bool preserveNS, bool preserveType) const;
-  virtual ASTNode* staticResolution(StaticContext *context);
-  virtual ASTNode *staticTyping(StaticContext *context);
   virtual PendingUpdateList createUpdateList(DynamicContext *context) const;
-
-  const VectorOfASTNodes &getChildren() const;
-
-private:
-  class ParenthesizedResult : public ResultImpl
-  {
-  public:
-    ParenthesizedResult(const XQParenthesizedExpr *di, int flags);
-
-    Item::Ptr next(DynamicContext *context);
-    std::string asString(DynamicContext *context, int indent) const;
-
-  private:
-    int _flags;
-    const XQParenthesizedExpr *_di;
-    VectorOfASTNodes::const_iterator _i;
-    Result _result;
-  };
-
-  VectorOfASTNodes _astNodes;
 };
 
 #endif
--- include/xqilla/functions/FunctionTrace.hpp
+++ include/xqilla/functions/FunctionTrace.hpp
@@ -19,12 +19,6 @@
  * $Id$
  */
 
-/*
-
-  Trace function
-
-*/
-
 #ifndef _FUNCTIONTRACE_HPP
 #define _FUNCTIONTRACE_HPP
 
@@ -41,13 +35,13 @@ public:
 
   FunctionTrace(const VectorOfASTNodes &args, XPath2MemoryManager* memMgr);
 
-  virtual ASTNode* staticResolution(StaticContext *context);
+  virtual ASTNode *staticResolution(StaticContext *context);
   virtual ASTNode *staticTyping(StaticContext *context);
 
   Sequence createSequence(DynamicContext* context, int flags=0) const;
 };
 
-#endif // _FUNCTIONTRACE_HPP
+#endif
 
 
 
