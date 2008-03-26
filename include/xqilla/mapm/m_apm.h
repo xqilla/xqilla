5ed79a854bab8ab64a1212f6ce949d380acc3f2b
George Feinberg <gmfeinberg@users.sourceforge.net>
2008-03-26 17:14:37 +0000

    Eliminate warning from HP-UX C++ compiler

--- include/xqilla/mapm/m_apm.h
+++ include/xqilla/mapm/m_apm.h
@@ -320,7 +320,7 @@ public:
 	/* Destructor */
 	~MAPM();
 
-  const M_APM c_struct() const { return myVal; }
+	M_APM c_struct() const { return myVal; }
 	
 	/* Extracting string descriptions: */
 	void toString(char *dest,int decimalPlaces) const;
