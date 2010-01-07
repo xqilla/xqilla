79f12f066a7ac81121ee89d9304334eaa7eac1d2
John Snelson <jpcs@users.sourceforge.net>
2010-01-07 17:23:13 +0000

    Updated the copyright notices for 2010.

--- include/xqilla/functions/FunctionPower.hpp
+++ include/xqilla/functions/FunctionPower.hpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2004-2009
- *     Oracle. All rights reserved.
+ * Copyright (c) 2004, 2010,
+ *     Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
