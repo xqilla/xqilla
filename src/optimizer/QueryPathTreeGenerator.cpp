c57f876564e14efc0444805695bf04be7a941bd8
John Snelson <john.snelson@marklogic.com>
2011-04-07 12:52:01 +0100

    Fixed a bug with inline functions and document projection
    --HG--
    branch : xqilla_2_3

--- src/optimizer/QueryPathTreeGenerator.cpp
+++ src/optimizer/QueryPathTreeGenerator.cpp
@@ -1466,7 +1466,7 @@ ASTNode *QueryPathTreeGenerator::optimizeInlineFunction(XQInlineFunction *item)
     }
   }
 
-  generate(item->getInstance());
+  generate(item->getInstance()).markSubtreeResult();
 
   if(params) {
     delete varStore_.popScope();
