140761e38009a69bee30aa16cdfbf1072cf7cbff
Rucong Zhao <rucong.zhao@oracle.com>
2011-06-28 21:20:36 -0700

    Add rounding operations to NumUtils::MAPMtoInt()
    --HG--
    branch : xqilla_2_3

--- src/utils/NumUtils.cpp
+++ src/utils/NumUtils.cpp
@@ -23,7 +23,23 @@
 
 int NumUtils::MAPMtoInt(MAPM m)
 {
-  return (int)m.toDouble();
+  /*
+   * In theory no float unit can provide absolutely accuracy
+   * (http://en.wikipedia.org/wiki/Floating_point#Accuracy_problems). Says the 
+   * double value is 3, in computer the actual value might be 2.9999999996.
+   * In this case, if we convert it to integer directly, some compilers would 
+   * conver the value to "2", not expected "3".
+   *
+   * This phenomenon is not a bug but just due to float unit/arithmetic
+   * accuracy limitation, especially on those imprecise float units like
+   * embedded systems and float unit emulators. 
+   *
+   * To solve this accuracy issue, we round the double value with 0.5 before 
+   * converting it to integer. This method is a common solution for safely
+   * converting double value to integer. 
+   */
+  double x = m.toDouble();
+  return (x >= 0 ? (int)(x + 0.5) : (int)(x - 0.5));
 }   
     
 double NumUtils::MAPMtoDouble(MAPM m)
