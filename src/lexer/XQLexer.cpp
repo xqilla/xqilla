ae3809fb7b10210187d0c07f6728b500ea279c06
Lauren Foutz <lauren.foutz@oracle.com>
2015-05-01 08:55:38 -0700

    [] Updating the copyright ahead of a new patch release.
    --HG--
    branch : xqilla_2_3

--- src/lexer/XQLexer.cpp
+++ src/lexer/XQLexer.cpp
@@ -13819,8 +13819,8 @@ static yyconst short int yy_chk[26368] =
 /*
  * Copyright (c) 2001, 2008,
  *     DecisionSoft Limited. All rights reserved.
- * Copyright (c) 2004, 2011,
- *     Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2015 Oracle and/or its affiliates. All rights reserved.
+ *     
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
